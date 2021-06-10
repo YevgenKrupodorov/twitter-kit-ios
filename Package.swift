23e536e4e6d7fcd9e26e064f093dcd927c2d9a9a
Alqueraf <alqueraf@gmail.com>
2021-06-10 16:04:22 +0200

    Document zip checksum

--- Package.swift
+++ Package.swift
@@ -19,8 +19,7 @@ let package = Package(
     targets: [
         
         // https://github.com/ResultsDirect/TwitterKit/blob/main/Package.swift
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Get checksum using "shasum -a 256 TwitterKit.zip"
         .binaryTarget(
                 name: "TwitterKit",
                 url: "https://github.com/Alqueraf/twitter-kit-ios/releases/download/v5.3.4/TwitterKit.zip",
