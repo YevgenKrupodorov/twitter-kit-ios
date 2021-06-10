e47b4fd2095eb50ab72bf546e47bbc11bab75757
Alqueraf <alqueraf@gmail.com>
2021-06-10 14:46:09 +0200

    Set binary target to Package.swift

--- Package.swift
+++ Package.swift
@@ -16,10 +16,12 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+        // https://github.com/ResultsDirect/TwitterKit/blob/main/Package.swift
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "TwitterKit",
-            dependencies: []),
+        .binaryTarget(
+                name: "TwitterKit",
+                url: "https://github.com/Alqueraf/twitter-kit-ios/releases/download/v5.3.4/TwitterKit.zip",
+            ),
     ]
 )
