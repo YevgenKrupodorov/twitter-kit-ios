8038fbf8483ee9237d7f958275f25d53902dfe42
Alqueraf <alqueraf@gmail.com>
2021-06-10 15:14:11 +0200

    Set Platform to Package

--- Package.swift
+++ Package.swift
@@ -5,11 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "TwitterKit",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "TwitterKit",
-            targets: ["TwitterKit"]),
+        .library(name: "TwitterKit", targets: ["TwitterKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -17,7 +18,7 @@ let package = Package(
     ],
     targets: [
         
-        // https://github.com/Alqueraf/twitter-kit-ios/releases/download/v5.3.4/TwitterKit.zip
+        // https://github.com/ResultsDirect/TwitterKit/blob/main/Package.swift
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
