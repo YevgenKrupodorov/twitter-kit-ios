a8b9ee4ba524470160b60b8f0b0353ee3287fee9
Alqueraf <alqueraf@gmail.com>
2021-06-10 15:04:53 +0200

    Add checksum to package

--- Package.swift
+++ Package.swift
@@ -16,12 +16,14 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // https://github.com/ResultsDirect/TwitterKit/blob/main/Package.swift
+        
+        // https://github.com/Alqueraf/twitter-kit-ios/releases/download/v5.3.4/TwitterKit.zip
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
                 name: "TwitterKit",
-                url: "https://github.com/Alqueraf/twitter-kit-ios/releases/download/v5.3.4/TwitterKit.zip"
+                url: "https://github.com/Alqueraf/twitter-kit-ios/releases/download/v5.3.4/TwitterKit.zip",
+                checksum: "aa6dd80ca360e0cde4005813534e4fbdfff4772f5e21cde362bb40f378a425ca"
             ),
     ]
 )
